ccd14be002584c1a38500138c7c6ee027f5bc6c3
zakir Hossen <zakirhossen082@gmail.com>
2025-04-08 11:30:24 +0600

    find single | multiple data from database

--- index.js
+++ index.js
@@ -72,3 +72,43 @@ app.listen(port, async ()=>{
 console.log(`server is running at http://localhost:${port}`);
   await connectDB();
 });
+
+
+//route for products read from browser
+app.get('/products', async (req,res)=>{
+    try{
+        const products = await Product.find();
+        if(products){
+            res.status(200).send(products);
+        }else{
+            res.status(404).send({
+                message: 'Product not found'
+            });
+        }
+
+    }catch(error){
+        res.status(500).send({message: error.message});
+    }
+});
+
+//route for single product find from browser
+app.get('/products/:id', async (req,res)=>{
+    try{
+        const id = req.params.id;
+        // const products = await Product.findOne({_id:id}).select({_id:0,title:1}); 
+        //1 =true, 0=false
+
+         //another way to skip some field data
+        const product = await Product.findOne({_id:id},{_id:0,title:1}); 
+        if(product){
+            res.status(200).send(product);
+        }else{
+            res.status(404).send({
+                message: 'Product not found'
+            });
+        }
+
+    }catch(error){
+        res.status(500).send({message: error.message});
+    }
+});
