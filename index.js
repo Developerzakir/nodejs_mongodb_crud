14b0579b62a78e62ddd6295ff229fe8c7feb0ac4
zakir Hossen <zakirhossen082@gmail.com>
2025-04-08 10:24:35 +0600

    mongoose schema & model create

--- index.js
+++ index.js
@@ -6,18 +6,25 @@ const app = express();
 const port = 3002;
 
 
-//One way to connect mongodb
-// mongoose.connect('mongodb://127.0.0.1:27017/products')
-// .then(()=>console.log("db is connected"))
-// .catch((error)=>{
-//     console.log("db not connected");
-//     console.log(error)
-//     process.exit(1)
-// });
+//create product schema 
+
+const productSchema = new mongoose.Schema({
+  title: String,
+  price: Number,
+  description: String,
+  createdAt:{
+    type: Date,
+    default: Date.now
+  }
+});
+
+//create product model
+const Product = mongoose.model("Products",productSchema);
+
 
 const connectDB = async ()=>{
     try{
-       await mongoose.connect('mongodb://127.0.0.1:27017/products');
+       await mongoose.connect('mongodb://127.0.0.1:27017/productdb');
        console.log("db is connected");
     }catch(error){
         console.log("db not connected");
